4d2f1dacd2346d8ab6bfdbb87e9bdcefbe5ecce6
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2023-09-20 10:19:04 +0300

    Display incoming garbage in the SVG

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import {maxDropletStrategy2} from './ai';
+import { WIDTH } from './bitboard';
 import {SimpleGame} from './game';
 import {SimplePuyoScreen} from './screen';
 
@@ -7,6 +8,60 @@ export * from './bitboard';
 export * from './game';
 export * from './jkiss';
 export * from './screen';
+
+const SMALL = 1;
+const LARGE = 6;
+const ROCK = 30;
+const STAR = 180;
+const MOON = 360;
+const CROWN = 720;
+const COMET = 1440;
+
+export function garbageDisplay(amount: number): string[] {
+  const result: string[] = [];
+  while (amount >= COMET) {
+    amount -= COMET;
+    result.push("comet");
+  }
+  while (amount >= CROWN) {
+    amount -= CROWN;
+    result.push("crown");
+  }
+  while (amount >= MOON) {
+    amount -= MOON;
+    result.push("moon");
+  }
+  while (amount >= STAR) {
+    amount -= STAR;
+    result.push("star");
+  }
+  while (amount >= ROCK) {
+    amount -= ROCK;
+    result.push("rock");
+  }
+  while (amount >= LARGE) {
+    amount -= LARGE;
+    result.push("large");
+  }
+  while (amount >= SMALL) {
+    amount -= SMALL;
+    result.push("small");
+  }
+  return result;
+}
+
+export function combinedGarbageDisplay(pendingGarbage: number, lateGarbage:number): string[] {
+  const pending = garbageDisplay(pendingGarbage);
+  const late = garbageDisplay(lateGarbage);
+  const result = pending.concat(late);
+  while (result.length > WIDTH && result.includes("small")) {
+    result.splice(result.indexOf("small"), 1);
+  }
+  if (result.length <= WIDTH) {
+    return result;
+  }
+  return garbageDisplay(pendingGarbage + lateGarbage).slice(0, WIDTH);
+}
 
 export function benchmark() {
   const screen = new SimplePuyoScreen();
