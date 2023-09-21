8a5a65f868299a858505b283b538536a4659d688
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2023-09-21 20:49:28 +0300

    Run max droplet strategy depth=2 in a dedicated worker thread

---
+++ lib/worker.ts
@@ -0,0 +1,21 @@
+import {SimpleGame, SimplePuyoScreen, maxDropletStrategy2} from '../src';
+
+onmessage = e => {
+  // Revive the class instance.
+  const screen = new SimplePuyoScreen();
+  screen.grid = e.data.screen.grid;
+  screen.chainNumber = e.data.screen.chainNumber;
+  screen.bufferedGarbage = e.data.screen.bufferedGarbage;
+  screen.garbageSlots = e.data.screen.garbageSlots;
+  const game = new SimpleGame(
+    screen,
+    e.data.pendingGarbage,
+    e.data.lateGarbage,
+    e.data.lateTimeRemaining,
+    e.data.colorSelection,
+    e.data.bag
+  );
+  const strategy = maxDropletStrategy2(game);
+
+  postMessage(strategy);
+};
